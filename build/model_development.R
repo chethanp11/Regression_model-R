7884aa5e5c72d941c73c9dcb9645f3d754208e59
Chethan Puttegowda <chethanp11@gmail.com>
2018-02-05 15:08:44 +0530

    Update model_development.R

--- build/model_development.R
+++ build/model_development.R
@@ -3,12 +3,12 @@
 #### SIMPLE LINEAR REGRESSION #################
 
 
-
 ## Importing the dataset #############
 ## Importing the dataset #############
 setwd("D:\\Knowledge\\MACHINE_LEARNING_TOTAL\\04.1.1 Linear_Regression")
 getwd()
 dir()
+
 ls()
 # Alt+Shift+K gives cheat sheet
 # Slelect function and hit F1 for help - shortcut
